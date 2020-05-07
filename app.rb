4cac5c5869935258953018ebc6104d6c67d397a8
dlyons8 <dlyons8@binghamton.edu>
2020-05-07 10:13:40 +0300

    removed class app

--- app.rb
+++ app.rb
@@ -1,11 +1,9 @@
 require 'sinatra'
 require 'bitx'
 
-class App < Sinatra::Base
-  get '/' do
-    @btc = BitX.ticker('XBTZAR')[:ask]
-    @eth = BitX.ticker('ETHZAR')[:ask] 
-    @xrp = BitX.ticker('XRPZAR')[:ask]
-    erb :index
-  end
+get '/' do
+  @btc = BitX.ticker('XBTZAR')[:ask]
+  @eth = BitX.ticker('ETHZAR')[:ask] 
+  @xrp = BitX.ticker('XRPZAR')[:ask]
+  erb :index
 end
